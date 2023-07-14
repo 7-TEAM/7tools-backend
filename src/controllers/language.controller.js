be387f4cb8ebd90026d609cbef54cb5dc3d8f049
Grewather <69985703+Grewather@users.noreply.github.com>
2023-07-14 20:28:41 +0200

    returns platform and versions

--- src/controllers/language.controller.js
+++ src/controllers/language.controller.js
@@ -1,15 +1,34 @@
-const languageUtil = require('../utils/language.util')
+// ===========  lIPTA CODE ===========
+// const languageUtil = require("../utils/language.util");
+
+// const languageController = {
+//   get: (req, res) => {
+//     languageUtil
+//       .getDownloadUrls(
+//         "https://www.ruby-lang.org/en/downloads/releases/",
+//         "https://cache.ruby-lang.org/pub/ruby/"
+//       )
+//       .then((releases) => {
+//         res.send(releases);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   },
+// };
+
+// module.exports = languageController;
+// ===========  greW CODE ===========
 
 const languageController = {
-    get: (req, res) => {
-        languageUtil.getDownloadUrls('https://www.ruby-lang.org/en/downloads/releases/', 'https://cache.ruby-lang.org/pub/ruby/')
-        .then(releases => {
-            res.send(releases)
-        })
-        .catch(error => {
-            console.error(error)
-        });
-    }
-}
-  
-module.exports = languageController
+  get: (req, res) => {
+    const platform = req.query.platform;
+    const version = req.query.version;
+    res.json({
+      platform: platform,
+      version: version,
+    });
+  },
+};
+
+module.exports = languageController;
--- src/controllers/languages.controller.js
+++ src/controllers/languages.controller.js
@@ -1,15 +1,31 @@
-const languageUtil = require('../utils/language.util')
+// ===========  LPTA CODE ===========
+
+// const languageUtil = require('../utils/language.util')
+
+// const languagesController = {
+//     get: (req, res) => {
+//         languageUtil.getLatestDownloadUrl('https://www.ruby-lang.org/en/downloads/releases/', 'https://cache.ruby-lang.org/pub/ruby/', /\/pub\/ruby\/([\d.]+)/)
+//         .then(latestReleaseLink => {
+//             res.send(latestReleaseLink)
+//         })
+//         .catch(error => {
+//             console.error(error)
+//         });
+//     }
+// }
+
+// module.exports = languagesController;
+// ===========  greW CODE ===========
 
 const languagesController = {
-    get: (req, res) => {
-        languageUtil.getLatestDownloadUrl('https://www.ruby-lang.org/en/downloads/releases/', 'https://cache.ruby-lang.org/pub/ruby/', /\/pub\/ruby\/([\d.]+)/)
-        .then(latestReleaseLink => {
-            res.send(latestReleaseLink)
-        })
-        .catch(error => {
-            console.error(error)
-        });
-    }
-}
+  get: (req, res) => {
+    const platform = req.query.platform;
+    const version = req.query.version;
+    res.json({
+      platform: platform,
+      version: version,
+    });
+  },
+};
 
 module.exports = languagesController;
